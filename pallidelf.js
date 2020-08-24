d3c7f0e8a1208adbe3b94efb12488a80f8dd037e
sh-tney <joshwhitney789@gmail.com>
2020-08-24 19:02:02 +1200

    Finished up Pallid Elf

--- pallidelf.js
+++ pallidelf.js
@@ -2,26 +2,26 @@ var iFileName = "Shitney's Pallid Elf.js";
 RequiredSheetVersion(12.999);
 
 // Define the source
-SourceList.EGW={
-	name : "Player's Handbook",
-	abbreviation : "EGW",
-	group : "Explorer's Guide to Wildemount",
-	url : "",
-	date : "2020/08/03"
+SourceList.EGtW={
+	name : "Explorer's Guide to Wildemount",
+	abbreviation : "EGtW",
+	group : "Primary Sources",
+	url : "https://dnd.wizards.com/products/wildemount",
+	date : "2020/03/17"
 };
 
 RaceList["pallid elf"] = {
 	regExpSearch : /^(?!.*half)((?=.*(pallid))|((?=.*\b(elfs?|elves|elvish|elven)\b))).*$/i,
 	name : "Pallid Elf",
 	sortname : "Elf, Pallid",
-	source : ["EGW", 24],
+	source : ["EGtW", 200],
 	plural : "Pallid Elves",
 	size : 3,
 	speed : {
 		walk : { spd : 30, enc : 20 }
 	},
 	languageProfs : ["Common", "Elvish"],
-	vision : ["Darkvision", 60],
+	vision : [["Darkvision", 60],["Adv. on Intelligence (Investigation) and Wisdom (Insight) checks", ""]],
 	savetxt : {
 		text : ["Magic can't put me to sleep"],
 		adv_vs : ["charmed"]
@@ -29,12 +29,12 @@ RaceList["pallid elf"] = {
 	skills : ["Perception"],
 	age : " typically claim adulthood around age 100 and can live to be 750 years old",
 	height : " range from under 5 to over 6 feet tall (4'6\" + 2d10\")",
-	weight : " weigh around 100 lb (75 + 2d6 \xD7 1d6 lb)",
-	heightMetric : " range from under 1,5 to 1,7 metres tall (135 + 5d6 cm)",
-	weightMetric : " weigh around 45 kg (35 + 5d6 \xD7 2d6 / 10 kg)",
-	improvements : "Drow: +2 Dexterity, +1 Wisdom;",
+	weight : " weigh around 110 lb (90 + 2d10 \xD7 1d4 lb)",
+	heightMetric : " range from under 1,5 to 1,7 metres tall (137 + 5d10 cm)",
+	weightMetric : " weigh around 50 kg (40 + 5d10 \xD7 2d4 / 10 kg)",
+	improvements : "Pallid Elf: +2 Dexterity, +1 Wisdom;",
 	scores : [0, 2, 0, 0, 1, 0],
-	trait : "Pallid Elf (+2 Dexterity, +1 Wisdom)\nTrance: Elves don't need to sleep, but meditate semiconsciously, for 4 hours a day. This gives the same benefit as a human gets from 8 hours of sleep (long rest takes only 4 hours).\nIncisive Sense: I have advantage on Intelligence (Investigation) and Wisdom (Insight) checks.\nBlessing of the Moonweaver: 1st level: Light cantrip; 3rd level: Sleep; 5th level: Invisibility. Both spells can be used once per long rest. Wisdom is my spellcasting ability for these.", // errata to specify once per day is long rest
+	trait : "Pallid Elf (+2 Dexterity, +1 Wisdom)\nTrance: Elves don't need to sleep, but meditate semiconsciously, for 4 hours a day. This gives the same benefit as a human gets from 8 hours of sleep (long rest takes only 4 hours).\nIncisive Sense: I have advantage on Intelligence (Investigation) and Wisdom (Insight) checks.\nBlessing of the Moonweaver: 1st level: Light cantrip; 3rd level: Sleep; 5th level: Invisibility. I can cast both spells on myself once per long rest without material components, using Wisdom as my spellcasting ability for these.", // errata to specify once per day is long rest
 	abilitySave : 5,
 	spellcastingAbility : 5,
 	spellcastingBonus : {
@@ -59,7 +59,7 @@ RaceList["pallid elf"] = {
 			}
 		},
 		"invisibility" : {
-			name : "Invisibility",
+			name : "Invisibility (self only)",
 			minlevel : 5,
 			usages : 1,
 			recovery : "long rest",
